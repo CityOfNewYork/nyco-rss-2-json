a3211e12e7be5bac0e3c3dfd33f59b4abb52e21c
devowhippit <devonhirth@gmail.com>
2019-12-12 13:45:45 -0500

    Convert indentation to spaces

--- index.js
+++ index.js
@@ -4,64 +4,64 @@ const xmltojs = require('xml2js').parseString;
 // const junkData = require('response.js');
 
 const sendRes = (status, body) => {
-	const response = {
-		isBase64Encoded: true | false,
-		statusCode: status,
-		headers: {
+  const response = {
+    isBase64Encoded: true | false,
+    statusCode: status,
+    headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
-		},
-		body: JSON.stringify(body),
-	};
+    },
+    body: JSON.stringify(body),
+  };
 
-	return response;
+  return response;
 };
 
 const isObject = obj => typeof obj === 'object' && !Array.isArray(obj);
 
 const walk = (array, fn) => {
-	return array.map((item) => {
-		item = fn(item);
+  return array.map((item) => {
+    item = fn(item);
 
-		if (isObject(item)) {
-			Object.keys(item).map((i) => {
-				item[i] = fn(item[i]);
-				item[i] = (Array.isArray(item[i])) ? walk(item[i], fn) : item[i];
-			});
+    if (isObject(item)) {
+      Object.keys(item).map((i) => {
+        item[i] = fn(item[i]);
+        item[i] = (Array.isArray(item[i])) ? walk(item[i], fn) : item[i];
+      });
 
-		}
+    }
 
-		return (Array.isArray(item)) ? walk(item,fn) : item;
-	});
+    return (Array.isArray(item)) ? walk(item,fn) : item;
+  });
 };
 
 const formatter = (item) => {
-	if (Array.isArray(item) && item.length === 1 && !isObject(item[0])) {
-		item = item[0];
-	} else if (Array.isArray(item) && isObject(item[0])) {
-		cleanUpJsonItem(item[0]);
-	} else if (Array.isArray(item)){
-		item.forEach(element => {
-			if (typeof element === 'string') {
-				element = element;
-			}
-		});
-	}  else {
-		item = item;
-	}
-	return item;
+  if (Array.isArray(item) && item.length === 1 && !isObject(item[0])) {
+    item = item[0];
+  } else if (Array.isArray(item) && isObject(item[0])) {
+    cleanUpJsonItem(item[0]);
+  } else if (Array.isArray(item)){
+    item.forEach(element => {
+      if (typeof element === 'string') {
+        element = element;
+      }
+    });
+  }  else {
+    item = item;
+  }
+  return item;
 };
 
 const cleanUpJsonItem = jsonItem => {
-	Object.keys(jsonItem).forEach(key => {
-		if (Array.isArray(jsonItem[key]) && jsonItem[key].length === 1) {
-			jsonItem[key] = jsonItem[key].shift();
-		} else {
-			jsonItem[key] =   jsonItem[key];
-		}
+  Object.keys(jsonItem).forEach(key => {
+    if (Array.isArray(jsonItem[key]) && jsonItem[key].length === 1) {
+      jsonItem[key] = jsonItem[key].shift();
+    } else {
+      jsonItem[key] =   jsonItem[key];
+    }
 
-		return jsonItem;
-	});
+    return jsonItem;
+  });
 };
 
 exports.handler = async (event) => {
